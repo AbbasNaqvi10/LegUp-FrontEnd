ce0c783a19d4d09edf4f0339dfbac2581280637b
AbbasNaqvi10 <syedabbasnaqvi10@gmail.com>
2022-09-20 02:31:56 +0500

    dashboard added

--- components/navbar.js
+++ components/navbar.js
@@ -1,36 +1,102 @@
-import Link from "next/link";
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '../public/img/logo.png'
+import { useState } from 'react';
+
 
 export default function Navbar() {
+    const [navbar, setNavbar] = useState(false);
     return (
-        <>
-            <nav class="bg-white border-100 px-2 sm:px-4 py-2.5">
-                <div class="container flex flex-wrap justify-between items-center mx-auto">
-                    <h1 class="text-blue bold">LegUp</h1>
-                    <div class="flex md:order-2">
-                        <a href="/account/login" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Join Us</a>
-                        <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
-                            <span class="sr-only">Open main menu</span>
-                            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                        </button>
+        <div>
+            <Head>
+                <title>LegUp</title>
+                {/* <meta
+                    name="description"
+                    content="Create Next JS Responsive Menu with Tailwind CSS"
+                /> */}
+                <link rel="icon" href="./img/logo.png" />
+            </Head>
+            <nav className="w-full ">
+                <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+                    <div>
+                        <div className="flex items-center justify-between py-3 md:py-4 md:block">
+                            <a href="/" >
+                                <h1 className='xs:text-black sm:text-green-900 md:text-red-900 lg:text-'>LegUp</h1>
+                                {/* <Image
+                                src={Logo}
+                                alt='LegUp'
+                                /> */}
+                            </a>
+                            <div className="md:hidden">
+                                <button
+                                    className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                    onClick={() => setNavbar(!navbar)}
+                                >
+                                    {navbar ? (
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="w-6 h-6 text-blue-700"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                    ) : (
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="w-6 h-6 text-blue-700"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                            strokeWidth={2}
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M4 6h16M4 12h16M4 18h16"
+                                            />
+                                        </svg>
+                                    )}
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                    <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-                        <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
-                            <li>
-                                <a href="/home" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-blue md:dark:hover:bg-transparent dark:border-gray-700">For Landers</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-blue md:dark:hover:bg-transparent dark:border-gray-700">For Consumers</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-blue md:dark:hover:bg-transparent dark:border-gray-700">About Us</a>
-                            </li>
-                        </ul>
+                    <div>
+                        <div
+                            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'block' : 'hidden'
+                                }`}
+                        >
+                            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                                <li className="text-blue-700">
+                                    <Link href="/">
+                                        <a>Home</a>
+                                    </Link>
+                                </li>
+                                <li className="text-blue-700">
+                                    <Link href="#">
+                                        <a>Lander</a>
+                                    </Link>
+                                </li>
+                                <li className="text-blue-700">
+                                    <Link href="#">
+                                        <a>Consumer</a>
+                                    </Link>
+                                </li>
+                                <li className="text-blue-700">
+                                    <Link href="#">
+                                        <a>About US</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </nav>
-        </>
-    )
+        </div>
+    );
 }
